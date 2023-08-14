18fcbb91584d791afff7339a6a59b128b1942ea7
Nurbolat <nurba.dav@gmail.com>
2023-08-14 18:50:43 +0600

    attempt 1

--- src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
+++ src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
@@ -4,6 +4,23 @@ import java.util.HashSet;
 import java.util.List;
 
 public class HashSetCreator {
+    /* If the element x from List<Integer> is even, then add to the HashSet values: x, x/2, (x/2)/2 ... ((x/2)/2)../2) (dividing by 2 until the last inserted element is an odd number)
+     * If the element x from List<Integer> is odd, then add to the HashSetvalues: x and 2x
+     */
     public HashSet<Integer> createHashSet(List<Integer> sourceList) {
+        HashSet<Integer> integers = new HashSet<>();
+        for (Integer element : sourceList) {
+            if (element % 2 == 1) {
+                integers.add(element);
+                integers.add(element * 2);
+                continue;
+            }
+            while (element % 2 == 0) {
+                integers.add(element);
+                element /= 2;
+            }
+            integers.add(element);
+        }
+        return integers;
     }
 }
--- src/main/java/com/epam/mjc/collections/set/SetCombinationCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SetCombinationCreator.java
@@ -2,8 +2,13 @@ package com.epam.mjc.collections.set;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class SetCombinationCreator {
     public Set<String> createSetCombination(Set<String> firstSet, Set<String> secondSet, Set<String> thirdSet) {
+        Set<String> result = new HashSet<>(firstSet);
+        result = result.stream().filter(s -> secondSet.contains(s) && !thirdSet.contains(s)).collect(Collectors.toSet());
+        result.addAll(thirdSet.stream().filter(s -> !(firstSet.contains(s) || secondSet.contains(s))).collect(Collectors.toSet()));
+        return result;
     }
 }
